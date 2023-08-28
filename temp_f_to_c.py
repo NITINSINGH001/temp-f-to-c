59f0fe24d73bcc31166556dad657cc48a527374f
NITINSINGH001 <143018967+NITINSINGH001@users.noreply.github.com>
2023-08-28 22:15:51 +0530

    Add files via upload

---
+++ temp_f_to_c.py
@@ -0,0 +1,8 @@
+#input section
+farh = float(input("Enter the Temperature in fahrenheit: "))
+
+#logic section
+cel = (farh - 32) * 0.5
+
+#disply section
+print("The converted temperature from fahrenheit to celsius : ",cel)
